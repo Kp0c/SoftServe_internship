55ba5fb0cd8bbf8382e76bf4ad0685b081f865a7
Kp0c <1111andrey1111@gmail.com>
2017-07-12 11:07:08 +0300

    String calculator kata

---
+++ C#/StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private static List<string> GetDelimiters(string args)
+        {
+            List<string> delimiters = new List<string>();
+            GroupCollection delimiterGroups = Regex.Match(args, @"//(.+)\n").Groups;
+
+            if (delimiterGroups.Count > 1)
+            {
+                string delimiterString = delimiterGroups[0].Value;
+
+                MatchCollection delimiterMatches = Regex.Matches(delimiterString, @"\[([^[]*)\]");
+
+                foreach (Match match in delimiterMatches)
+                {
+                    delimiters.Add(match.Groups[1].Value);
+                }
+            }
+            return delimiters;
+        }
+
+        public static int Add(string args)
+        {
+            List<string> delimiters = new List<string>() { "\n" };
+            
+            if (args.Contains(@"//"))
+            {
+                List<string> userDelimiters = GetDelimiters(args);
+                delimiters.AddRange(userDelimiters);
+                args = args.Substring(args.IndexOf("\n"));
+            }
+            else
+            {
+                delimiters.Add(",");
+            }
+
+            int[] numbers = args.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(num => Convert.ToInt32(num))
+                .Where(n => n < 1000)
+                .ToArray();
+
+            int sum = 0;
+
+            int[] negatives = numbers.Where(n => n < 0).ToArray();
+
+            if(numbers.Any(n => n < 0))
+            {
+                throw new ArgumentException("negatives not allowed: " + numbers
+                    .Where(n => n < 0)
+                    .Select(n => n.ToString())
+                    .Aggregate((current, next) => current + ", " + next));
+            }
+
+            foreach (int number in numbers)
+            {
+                if(number < 0)
+                {
+                }
+
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ C#/StringCalculator/StringCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString_Returns0()
+        {
+            Assert.AreEqual(0, Calculator.Add(String.Empty));
+        }
+
+        [Test]
+        public void Add_OneArgument_ReturnsThisArgument()
+        {
+            Assert.AreEqual(1, Calculator.Add("1"));
+        }
+
+        [Test]
+        public void Add_TwoArguments_ReturnsSum()
+        {
+            Assert.AreEqual(10, Calculator.Add("5,5"));
+        }
+
+        [Test]
+        public void Add_FiveArguments_ReturnsSum()
+        {
+            Assert.AreEqual(25, Calculator.Add("5,5,5,5,5"));
+        }
+
+        [Test]
+        public void Add_NewLineInArguments_ReturnsSum()
+        {
+            Assert.AreEqual(15, Calculator.Add("5\n5,5"));
+        }
+
+        [Test]
+        public void Add_SupportDifferentDelimiters_ReturnsSum()
+        {
+            Assert.AreEqual(500, Calculator.Add("//[;]\n50;450"));
+        }
+
+        [Test]
+        public void Add_SupportDelimitersBiggerThan1Char_ReturnsSum()
+        {
+            Assert.AreEqual(12, Calculator.Add("//[***]\n5***5***2"));
+        }
+
+        [Test]
+        public void Add_AllowMultipleDelimiters_ReturnsSum()
+        {
+            Assert.AreEqual(15, Calculator.Add("//[***][%%][$]\n5***5%%2$3"));
+        }
+
+        [Test]
+        public void Add_NegativeNumber_ThrowArgumentException()
+        {
+            Assert.That(() => { Calculator.Add("3, -5, -8"); }, Throws.TypeOf<ArgumentException>().And.Message.EqualTo("negatives not allowed: -5, -8"));
+        }
+
+        [Test]
+        public void Add_NumbersBiggerThan1000_IgnoreNumbersBiggerThan1000()
+        {
+            Assert.AreEqual(2, Calculator.Add("2,1001"));
+        }
+    }
+}
