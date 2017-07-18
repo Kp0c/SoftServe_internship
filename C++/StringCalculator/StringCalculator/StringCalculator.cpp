6dd2360af8bfe32e32adc7c722a1853272d2fd86
Kp0c <1111andrey1111@gmail.com>
2017-07-18 16:52:56 +0300

    TDD-kata1 done

--- C++/StringCalculator/StringCalculator/StringCalculator.cpp
+++ C++/StringCalculator/StringCalculator/StringCalculator.cpp
@@ -1,39 +1,79 @@
 #include "StringCalculator.h"
 #include <vector>
+#include <exception>
 
-int PositionOfFirstDelimiter(std::string string, std::vector<std::string> delimiters)
+struct DelimiterAppearance
 {
-    size_t minPos = std::string::npos;
+    size_t pos;
+    int length;
+};
+
+DelimiterAppearance PositionOfFirstDelimiter(std::string string, std::vector<std::string> delimiters)
+{
+    DelimiterAppearance minPosDelimiter{ std::string::npos, 0 };
     size_t pos = std::string::npos;
     for (std::string delim : delimiters)
     {
         pos = string.find(delim);
-        if (pos != std::string::npos && (pos < minPos || minPos == std::string::npos))
+        if (pos != std::string::npos && (pos < minPosDelimiter.pos || minPosDelimiter.pos == std::string::npos))
         {
-            minPos = pos;
+            minPosDelimiter.pos = pos;
+            minPosDelimiter.length = delim.length();
         }
     }
 
-    return minPos;
+    return minPosDelimiter;
 }
 
 std::vector<int> Split(std::string numbers, std::vector<std::string> delimiters)
 {
     std::vector<int> result;
 
-    size_t pos = 0;
-    while ((pos = PositionOfFirstDelimiter(numbers, delimiters)) != std::string::npos)
+    DelimiterAppearance pos;
+    while ((pos = PositionOfFirstDelimiter(numbers, delimiters)).pos != std::string::npos)
     {
-        result.push_back(std::stoi(numbers.substr(0, pos)));
-        numbers.erase(0, pos + 1);
+        if (std::stoi(numbers.substr(0, pos.pos)) < 1000)
+        {
+            result.push_back(std::stoi(numbers.substr(0, pos.pos)));
+        }
+
+        numbers.erase(0, pos.pos + pos.length);
     }
 
+    int number = 0;
     if (numbers.length() > 0)
-        result.push_back(std::stoi(numbers));
+    {
+        number = std::stoi(numbers);
+        if (number < 1000)
+        {
+            result.push_back(number);
+        }
+    }
 
     return result;
 }
 
+std::vector<std::string> GetDelimiters(std::string string)
+{
+    std::string delimiterString = string.substr(2, string.find("\n") - 2);
+
+    if (delimiterString.length() != 1)
+    {
+        std::vector<std::string> delimiters;
+        int pos = std::string::npos;
+        while ((pos = delimiterString.find("[")) != std::string::npos)
+        {
+            delimiters.push_back(delimiterString.substr(pos + 1, delimiterString.find("]")-pos-1));
+            delimiterString = delimiterString.substr(delimiterString.find("]") + 1);
+        }
+
+        return delimiters;
+    }
+    else
+    {
+        return{ delimiterString };
+    }
+}
 
 int Add(std::string numbers)
 {
@@ -42,14 +82,43 @@ int Add(std::string numbers)
         return 0;
     }
 
-    std::vector<std::string> delimiters{ "\n", "," };
-    std::vector<int> numbersV = Split(numbers, delimiters);
+    std::vector<std::string> delimiters;
 
-    int sum = 0;
-    for (int i : numbersV)
+    if (numbers.find("//") == 0)
     {
-        sum += i;
+        delimiters = GetDelimiters(numbers);
+        numbers = numbers.substr(numbers.find("\n") + 1);
+    }
+    else
+    {
+        delimiters.push_back(",");
     }
 
-    return sum;
+    delimiters.push_back("\n");
+
+    std::vector<int> numbersV = Split(numbers, delimiters);
+
+    std::string negatives;
+    for (int i : numbersV)
+    {
+        if (i < 0)
+        {
+            negatives += std::to_string(i) + ", ";
+        }
+    }
+
+    if (!negatives.empty())
+    {
+        throw std::invalid_argument("negatives not allowed: " + negatives.substr(0, negatives.length() - 2));
+    }
+    else
+    {
+        int sum = 0;
+        for (int i : numbersV)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
 }
--- C++/StringCalculator/StringCalculatorTest/Main.cpp
+++ C++/StringCalculator/StringCalculatorTest/Main.cpp
@@ -1,4 +1,5 @@
 #include <StringCalculator.h>
+#include <exception>
 #include <gtest\gtest.h>
 
 TEST(CalculatorAddTest, EmptyString)
@@ -29,6 +30,34 @@ TEST(CalculatorAddTest, NewLIneInsteadOfComma)
 TEST(CalculatorAddTest, SupportDifferentDelimiters)
 {
     EXPECT_EQ(6, Add("//;\n1;2;3"));
+}
+
+TEST(CalculatorAddTest, NegativeShouldThrowException)
+{
+    try 
+    {
+        Add("2,-5,-7");
+        FAIL();
+    }
+    catch (std::invalid_argument ex)
+    {
+        ASSERT_STREQ("negatives not allowed: -5, -7", ex.what());
+    }
+}
+
+TEST(CalculatorAddTest, IgnoreMoreThan1000)
+{
+    EXPECT_EQ(4, Add("//;\n1;1001;3"));
+}
+
+TEST(CalculatorAddTest, VariableLengthDelimiter)
+{
+    EXPECT_EQ(4, Add("//[***]\n1***3"));
+}
+
+TEST(CalculatorAddTest, AllowSeveralDelimiters)
+{
+    EXPECT_EQ(20, Add("//[***][$%][!]\n1***3$%6!10"));
 }
 
 int main(int argc, char **argv) {
