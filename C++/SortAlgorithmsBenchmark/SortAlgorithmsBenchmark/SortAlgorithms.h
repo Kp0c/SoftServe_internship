adeb427521215b6fb0a2b6feb628e6e31cca5793
Kp0c <1111andrey1111@gmail.com>
2017-07-20 15:14:52 +0300

    algorithm benchmarks

---
+++ C++/SortAlgorithmsBenchmark/SortAlgorithmsBenchmark/SortAlgorithms.h
@@ -0,0 +1,153 @@
+#pragma once
+#include <algorithm>
+#include <thread>
+
+void swap(int& a, int& b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+
+void BubbleSort(int count, int* arr)
+{
+    bool swappped = false;
+    for (int i = 0; i < count; ++i)
+    {
+        swappped = false;
+
+        for (int j = count - 1; j > i; --j)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+                swappped = true;
+            }
+        }
+
+        if (!swappped) break;
+    }
+}
+
+void Merge(int *a, int low, int high, int mid)
+{
+    int i, j, k;
+    int* temp = new int[high - low + 1];
+    i = low;
+    k = 0;
+    j = mid + 1;
+
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            temp[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            temp[k] = a[j];
+            k++;
+            j++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = a[i];
+        k++;
+        i++;
+    }
+
+    while (j <= high)
+    {
+        temp[k] = a[j];
+        k++;
+        j++;
+    }
+
+    for (i = low; i <= high; i++)
+    {
+        a[i] = temp[i - low];
+    }
+
+    delete[] temp;
+}
+
+void MergeSort(int *a, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+
+        MergeSort(a, low, mid);
+        MergeSort(a, mid + 1, high);
+
+        Merge(a, low, high, mid);
+    }
+}
+
+int Partition(int *arr, const int left, const int right)
+{
+    const int mid = left + (right - left) / 2;
+    const int pivot = arr[mid];
+
+    std::swap(arr[mid], arr[left]);
+    int i = left + 1;
+    int j = right;
+    while (i <= j) {
+        while (i <= j && arr[i] <= pivot) {
+            i++;
+        }
+
+        while (i <= j && arr[j] > pivot) {
+            j--;
+        }
+
+        if (i < j) {
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i - 1], arr[left]);
+    return i - 1;
+}
+
+void QuickSort(int *arr, const int left, const int right)
+{
+    if (left >= right) {
+        return;
+    }
+
+    int part = Partition(arr, left, right);
+
+    QuickSort(arr, left, part - 1);
+    QuickSort(arr, part + 1, right);
+}
+
+void QuickSortWithThreads(int* arr, const int left, const int right, const int size)
+{
+    if (left >= right) {
+        return;
+    }
+
+    int part = Partition(arr, left, right);
+
+    if (part - left > size/10)
+    {
+        std::thread t2(QuickSortWithThreads, arr, left, part - 1, size);
+        QuickSortWithThreads(arr, part + 1, right, size);
+        t2.join();
+    }
+    else
+    {
+        QuickSortWithThreads(arr, left, part - 1, size);
+        QuickSortWithThreads(arr, part + 1, right, size);
+    }
+}
+
+int compare(const void * a, const void * b)
+{
+    return (*(int*)a - *(int*)b);
+}
---
+++ C++/SortAlgorithmsBenchmark/SortAlgorithmsBenchmark/main.cpp
@@ -0,0 +1,53 @@
+#include "SortAlgorithms.h"
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <string>
+#include <functional>
+
+constexpr int COUNT = 100000;
+
+void benchMark(std::function<void(int* arr)> func, char* name, int* sampleArr)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    int* arr = new int[COUNT];
+    memcpy(arr, sampleArr, COUNT * sizeof(int));
+
+    func(arr);
+
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+
+    std::cout << name << std::endl;
+    std::cout << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl;
+}
+
+void BenchAll(int* sampleArr)
+{
+    std::function<void(int* arr)> bubbleSort(std::bind(BubbleSort, COUNT, std::placeholders::_1));
+    std::function<void(int* arr)> mergeSort(std::bind(MergeSort, std::placeholders::_1, 0, COUNT - 1));
+    std::function<void(int* arr)> stdQuickSort(std::bind(qsort, std::placeholders::_1, COUNT, sizeof(int), compare));
+    std::function<void(int* arr)> quickSort(std::bind(QuickSort, std::placeholders::_1, 0, COUNT - 1));
+    std::function<void(int* arr)> quickSortWithThreads(std::bind(QuickSortWithThreads, std::placeholders::_1, 0, COUNT - 1, COUNT));
+
+    if (COUNT <= 10000) benchMark(bubbleSort, "bubble sort", sampleArr);
+    benchMark(mergeSort, "merge sort", sampleArr);
+    benchMark(stdQuickSort, "std quick sort", sampleArr);
+    benchMark(quickSort, "quick sort", sampleArr);
+    benchMark(quickSortWithThreads, "quick sort with threads", sampleArr);
+}
+
+int main()
+{
+    int sampleArr[COUNT];
+
+    srand(time(nullptr));
+
+    for (int i = 0; i < COUNT; i++)
+    {
+        sampleArr[i] = rand()%1000;
+    }
+
+    BenchAll(sampleArr);
+}
