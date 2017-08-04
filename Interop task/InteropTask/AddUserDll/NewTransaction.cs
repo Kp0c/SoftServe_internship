401098c900854a573cd05ab2d80a32e71f76e8b0
Kp0c <1111andrey1111@gmail.com>
2017-08-04 17:04:00 +0300

    removed unused files

--- Interop task/InteropTask/AddUserDll/NewTransaction.cs
+++ Interop task/InteropTask/AddUserDll/NewTransaction.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Windows.Forms;
-
-namespace NewTransactionDll
+﻿namespace NewTransactionDll
 {
     public class NewTransaction
     {
--- Interop task/InteropTask/Tests/Header.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#import "msado15.dll" rename_namespace("ADO") rename("EOF", "EndOfFile") no_implementation
